53e4553a6b15d7d16540254584f36a4a73b2fe00
AradhyaTiwari10 <aradhyafcb@gmail.com>
2026-03-01 16:34:48 +0530

    feat(nlp): Configure TF-IDF Optimization with specific params

--- feature_engineering.py
+++ feature_engineering.py
@@ -1,6 +1,11 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 def extract_tfidf(corpus):
-    vectorizer = TfidfVectorizer(max_features=1500)
+    vectorizer = TfidfVectorizer(
+        max_features=2500,
+        min_df=2,
+        max_df=0.85,
+        ngram_range=(1,2)
+    )
     X = vectorizer.fit_transform(corpus)
     return X, vectorizer
